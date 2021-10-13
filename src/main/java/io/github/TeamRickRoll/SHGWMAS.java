6c52e625fc89f1e44a447868dcd1273fb8b78399
GoldenCookieGIT <sashaheichenko@icloud.com>
2021-10-13 10:13:42 -0700

    Basic Minestom server
    sus

---
+++ src/main/java/io/github/TeamRickRoll/SHGWMAS.java
@@ -0,0 +1,29 @@
+package io.github.TeamRickRoll;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+
+public class SHGWMAS {
+    public static void main(String[] args) {
+        MinecraftServer minestom = MinecraftServer.init();
+        MojangAuth.init();
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setChunkGenerator(new GeneratorDemo());
+
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+           final Player player = event.getPlayer();
+           event.setSpawningInstance(instanceContainer);
+           player.setRespawnPoint(new Pos(0, 50, 0));
+        });
+
+        minestom.start("0.0.0.0", 25565);
+    }
+}

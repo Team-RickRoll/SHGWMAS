b14a6fc02ff9769a4380dfe842f48b5707b74b07
GoldenCookieGIT <sashaheichenko@icloud.com>
2021-10-19 16:13:46 -0700

    Comments + more sound stuff

--- src/main/java/io/github/TeamRickRoll/SHGWMAS.java
+++ src/main/java/io/github/TeamRickRoll/SHGWMAS.java
@@ -11,30 +11,46 @@ import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.timer.SchedulerManager;
+import net.minestom.server.timer.Task;
 
 public class SHGWMAS {
     public static void main(String[] args) {
         MinecraftServer minestom = MinecraftServer.init();
+
+        // Mojang auth...
         MojangAuth.init();
+
+        // World Manager
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+
+        // The "world"
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+
+        // Using the demo generator
         instanceContainer.setChunkGenerator(new GeneratorDemo());
 
 
         MobController mobController = new MobController(instanceContainer);
+        SoundController soundController = new SoundController(instanceContainer);
+
 
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+
+        // Basic player login setup
         globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
             final Player player = event.getPlayer();
             event.setSpawningInstance(instanceContainer);
             player.setRespawnPoint(new Pos(0, 50, 0));
-
-
         });
+
+        // Right click block event used for debugging and testing
         globalEventHandler.addListener(PlayerBlockInteractEvent.class, event -> {
-            new SoundController().spookPlayer(event.getPlayer());
+            // Whooo it works!
+            soundController.soundFrenzy();
         });
 
+        // Stars the server, please dont put stuff under here :3
         minestom.start("0.0.0.0", 25565);
     }
 }
--- src/main/kotlin/io/github/TeamRickRoll/sounds/SoundController.kt
+++ src/main/kotlin/io/github/TeamRickRoll/sounds/SoundController.kt
@@ -1,23 +1,59 @@
 package io.github.TeamRickRoll.sounds
 
 import net.kyori.adventure.sound.Sound
+import net.minestom.server.MinecraftServer
 import net.minestom.server.entity.Player
+import net.minestom.server.instance.InstanceContainer
 import net.minestom.server.sound.SoundEvent
+import net.minestom.server.timer.SchedulerManager
+import net.minestom.server.timer.Task
+import net.minestom.server.utils.time.TimeUnit
 import kotlin.random.Random
 
-class SoundController {
+class SoundController(private val instanceContainer: InstanceContainer) {
+    private var currentTasks : List<Task>? = mutableListOf(); /* In case somehow multiple frenzies start, they will all stop correctly */
+    private val schedulerManager: SchedulerManager = MinecraftServer.getSchedulerManager();
+
     private val sounds: List<SoundEvent> = listOf(
         SoundEvent.AMBIENT_CAVE,
         SoundEvent.ENTITY_ZOMBIE_AMBIENT,
         SoundEvent.ENTITY_GHAST_AMBIENT,
-        SoundEvent.ENTITY_GHAST_DEATH,
         SoundEvent.ENTITY_GHAST_HURT,
         SoundEvent.ENTITY_GHAST_SCREAM,
         SoundEvent.ENTITY_GHAST_SHOOT,
         SoundEvent.ENTITY_CREEPER_PRIMED,
         SoundEvent.ENTITY_SKELETON_AMBIENT,
+        SoundEvent.ENTITY_ZOMBIE_ATTACK_WOODEN_DOOR,
     )
-    fun Player.spookPlayer(){
+
+
+    /* extention function (Player#spook() in kotlin, SoundController#spook(Player) in java */
+    private fun Player.spook(){
         playSound(Sound.sound(sounds[Random.nextInt(sounds.size)], Sound.Source.MASTER, 1f, 1f))
+    }
+
+
+
+    fun soundFrenzy(){
+        var i = 0;
+        /* adds the task to the list, no cancel() method like in bukkit, so we have to do it externally.
+         More complicated than it should be becuz im a bad programmer */
+        currentTasks = currentTasks?.plus(schedulerManager.buildTask {
+            // cancels after 60 sounds
+            if(i >= 60){
+                for(task in currentTasks!!){
+                    task.cancel()
+                }
+            }
+
+            // self-explanatory
+            for (player in instanceContainer.players){
+                // extension function explained above
+                player.spook()
+            }
+
+            i++
+
+        }.repeat(250, TimeUnit.MILLISECOND).schedule()) // repeats every 250 ms (1/4 of a second or 5 ticks)
     }
 }
